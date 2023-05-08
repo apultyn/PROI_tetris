7a0c26e90eaefac41dc4c78a7319fbd5bb003c2f
jkosla <01178539@pw.edu.pl>
2023-05-08 22:55:05 +0200

    Formatted main.

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,12 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include "playfield.h"
+#include "Tetrominoes.h"
+#include <iostream>
+#include "Game.h"
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
-    sf::RectangleShape rec;
-    rec.setSize(sf::Vector2f(10.0f, 20.0f));
-    rec.setFillColor(sf::Color::Cyan);
+    sf::RenderWindow window(sf::VideoMode(WIDTH * RESIZE * CELL_SIZE, VISIBLE_HEIGHT * RESIZE * CELL_SIZE), "Tetris");
+    Playfield playfield;
+    Tetrominoes piece;
+    piece.updateMatrix(playfield.getPlayfield()); // places a tetrominoe in matrix
+    Game game;
 
 
     while (window.isOpen())
@@ -17,11 +22,13 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        window.clear();
-        window.draw(rec);
+        window.clear();                 
+        game.drawPlayfield(playfield, window); // prints the matrix
         window.display();
 
     }
+
+
 
     return 0;
 }
