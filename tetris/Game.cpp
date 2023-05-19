bc42e56900d1abecd439961a04fe1843d659ddd3
jkosla <01178539@pw.edu.pl>
2023-05-19 21:13:09 +0200

    Changed getInit to getPos

--- tetris/Game.cpp
+++ tetris/Game.cpp
@@ -35,7 +35,7 @@ void Game::drawTetromino(Tetromino& piece, sf::RenderWindow& window)
         for (int j = 0; j < 4; j++)
         {
             if (piece.matrix[i][j] == 1) {
-            square.get()->setPosition(j * CELL_SIZE * RESIZE + piece.getInitX(), i * CELL_SIZE * RESIZE + piece.getInitY());
+            square.get()->setPosition((j + piece.getPosX()) * CELL_SIZE * RESIZE , (i + piece.getPosY()) * CELL_SIZE * RESIZE);
             square.get()->setOutlineThickness(-1);
             square.get()->setOutlineColor(sf::Color::Black);
             square.get()->setFillColor(sf::Color::Red);    
