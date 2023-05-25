f6c99d6828fdf6f5ea2e4e9fd00cfad76d6fa51d
jkosla <01178539@pw.edu.pl>
2023-05-25 21:14:12 +0200

    Rows deletion, score

--- tetris/playfield.h
+++ tetris/playfield.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "playfield_dimensions.h"
 #include "Tetromino.h"
+#include <vector>
 
 class Playfield
 {
@@ -11,12 +12,19 @@ public:
     void updateMatrix(const Tetromino& tetromino);
     bool checkGameOver();
     bool checkIfDelete();
+    void setRowsToDelete();
+    void deleteRows();
+    std::vector<int> getRowsToDelete();
+    int getScore();
+
 
     // checking if game is over
     // deleting a line(s)
     // checking if movement is possible
     // color will be represented by numbers 1,2,3...
 private:
+    std::vector<int> del_rows;
+    int score;
 	//enum field_status {FREE, FILLED};
 };
 
