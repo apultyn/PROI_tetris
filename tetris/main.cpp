c023f7c79ccb56573bae82937dbe2302dc1d1a7b
jkosla <01178539@pw.edu.pl>
2023-05-28 13:57:16 +0200

    Added responsive button

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -13,9 +13,56 @@ int main()
 
     Game game;
     Playfield playfield;
-    game.setPlayfield(playfield);
-    game.initWindow();
-    game.startGame();
+    sf::RenderWindow window(sf::VideoMode(WIDTH * RESIZE * CELL_SIZE * 2, VISIBLE_HEIGHT * RESIZE * CELL_SIZE), "Tetris");
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event; 
+        sf::Text text;
+        sf::Font font;
+        font.loadFromFile("Quicklime-Regular.ttf");
+        text.setFillColor(sf::Color::White);
+        text.setFont(font);
+        text.setString("TETRIS");
+        text.setCharacterSize(120);
+        text.setPosition({275, 100});
+        text.setLetterSpacing(2);
+
+        sf::FloatRect ret = text.getGlobalBounds();
+        sf::RectangleShape rect;
+        rect.setPosition(ret.left - 20, ret.top - 20);
+        rect.setSize({ret.width + 40, ret.height + 40});
+        rect.setFillColor(sf::Color::Transparent);
+        rect.setOutlineColor(sf::Color::White);
+        rect.setOutlineThickness(2);
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && (ret.contains(localPosition.x, localPosition.y)))
+        {
+            
+            text.setScale(0.8, 0.8);
+            rect.setScale(0.8, 0.8);
+            rect.setPosition(rect.getPosition().x * 1.1, rect.getPosition().y * 0.9);
+            text.setPosition(text.getPosition().x * 1.1, text.getPosition().y * 0.9); 
+            game.initWindow();
+            std::this_thread::sleep_for(std::chrono::milliseconds(50)); 
+            window.close();
+            game.startGame();
+           
+        }
+
+        window.draw(rect);
+        window.draw(text);
+        window.display();
+    }
+
 
     return 0;
 }
