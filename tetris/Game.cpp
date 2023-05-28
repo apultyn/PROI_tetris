a02e732e91e26eaf673c73efcbbfc170580d10df
jkosla <01178539@pw.edu.pl>
2023-05-28 13:56:48 +0200

    Close window when game is over

--- tetris/Game.cpp
+++ tetris/Game.cpp
@@ -279,7 +279,8 @@ void Game::startGame()
         if (playfield.checkGameOver())
         {
 
-            std::this_thread::sleep_for(std::chrono::seconds(10));
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            window->close();
         }
         window->display();
 
