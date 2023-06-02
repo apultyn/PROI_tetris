b49aa7d35e0fa7298d925e36d2892d0da3243d9b
jkosla <01178539@pw.edu.pl>
2023-06-02 15:59:03 +0200

    Fixed tests

--- Tetromino_test/Tetromino_test.cpp
+++ Tetromino_test/Tetromino_test.cpp
@@ -1,8 +1,22 @@
 #include "pch.h"
 #include "../tetris/Tetromino.cpp"
 #include "CppUnitTest.h"
+//#include <string>
+//#include "../SFML-2.5.1/include/SFML/Graphics.hpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+std::string toString(Tetromino& block) {
+	std::stringstream tostring;
+	tostring << '[';
+	for (const auto& row : block.matrix) {
+		for (const auto& val : row) {
+			tostring << val << ',';
+		}
+	}
+	tostring << ']';
+	return tostring.str();
+}
 
 namespace Tetrominotest
 {
@@ -11,60 +25,60 @@ namespace Tetrominotest
 	public:
 		TEST_METHOD(TransposeMatrixI)
 		{
-			Tetromino block = Tetromino('1');
+			Tetromino block = Tetromino(1);
 			block.transpose_matrix();
 			std::string expected = "[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixJ)
 		{
-			Tetromino block = Tetromino('2');
+			Tetromino block = Tetromino(2);
 			block.transpose_matrix();
-			std::string expected = "[1,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,1,1,0,0,0,1,0,0,0,1,0,0,0,0,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixL)
 		{
-			Tetromino block = Tetromino('3');
+			Tetromino block = Tetromino(3);
 			block.transpose_matrix();
-			std::string expected = "[0,1,0,0,0,1,0,0,1,1,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,0,1,0,0,0,1,0,0,1,1,0,0,0,0,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixO)
 		{
-			Tetromino block = Tetromino('4');
+			Tetromino block = Tetromino(4);
 			block.transpose_matrix();
 			std::string expected = "[0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixS)
 		{
-			Tetromino block = Tetromino('5');
+			Tetromino block = Tetromino(5);
 			block.transpose_matrix();
 			std::string expected = "[0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixT)
 		{
-			Tetromino block = Tetromino('6');
+			Tetromino block = Tetromino(6);
 			block.transpose_matrix();
 			std::string expected = "[0,0,1,0,0,1,1,0,0,0,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixZ)
 		{
-			Tetromino block = Tetromino('7');
+			Tetromino block = Tetromino(7);
 			block.transpose_matrix();
 			std::string expected = "[0,1,0,0,0,1,1,0,0,0,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(TransposeMatrixDoubleZ)
 		{
-			Tetromino block = Tetromino('7');
+			Tetromino block = Tetromino(7);
 			block.transpose_matrix();
 			block.transpose_matrix();
 			std::string expected = "[0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 	};
 	TEST_CLASS(TetrominoTestRotation)
@@ -72,148 +86,148 @@ namespace Tetrominotest
 	public:
 		TEST_METHOD(RotateLeftI)
 		{
-			Tetromino block = Tetromino('1');
+			Tetromino block = Tetromino(1);
 			block.rotate_left();
 			std::string expected = "[0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightI)
 		{
-			Tetromino block = Tetromino('1');
+			Tetromino block = Tetromino(1);
 			block.rotate_right();
 			std::string expected = "[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftJ)
 		{
-			Tetromino block = Tetromino('2');
+			Tetromino block = Tetromino(2);
 			block.rotate_left();
-			std::string expected = "[0,0,0,0,0,1,0,0,0,1,0,0,1,1,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,0,0,0,0,0,1,0,0,0,1,0,0,1,1,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightJ)
 		{
-			Tetromino block = Tetromino('2');
+			Tetromino block = Tetromino(2);
 			block.rotate_right();
-			std::string expected = "[0,0,1,1,0,0,1,0,0,0,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftL)
 		{
-			Tetromino block = Tetromino('3');
+			Tetromino block = Tetromino(3);
 			block.rotate_left();
-			std::string expected = "[0,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,0,0,0,0,1,1,0,0,0,1,0,0,0,1,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightL)
 		{
-			Tetromino block = Tetromino('3');
+			Tetromino block = Tetromino(3);
 			block.rotate_right();
-			std::string expected = "[0,0,1,0,0,0,1,0,0,0,1,1,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			std::string expected = "[0,1,0,0,0,1,0,0,0,1,1,0,0,0,0,0,]";
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftO)
 		{
-			Tetromino block = Tetromino('4');
+			Tetromino block = Tetromino(4);
 			block.rotate_left();
 			std::string expected = "[0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightO)
 		{
-			Tetromino block = Tetromino('4');
+			Tetromino block = Tetromino(4);
 			block.rotate_right();
 			std::string expected = "[0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftS)
 		{
-			Tetromino block = Tetromino('5');
+			Tetromino block = Tetromino(5);
 			block.rotate_left();
 			std::string expected = "[0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightS)
 		{
-			Tetromino block = Tetromino('5');
+			Tetromino block = Tetromino(5);
 			block.rotate_right();
 			std::string expected = "[0,1,0,0,0,1,1,0,0,0,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftT)
 		{
-			Tetromino block = Tetromino('6');
+			Tetromino block = Tetromino(6);
 			block.rotate_left();
 			std::string expected = "[0,0,0,0,0,0,1,0,0,1,1,0,0,0,1,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightT)
 		{
-			Tetromino block = Tetromino('6');
+			Tetromino block = Tetromino(6);
 			block.rotate_right();
 			std::string expected = "[0,1,0,0,0,1,1,0,0,1,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateLeftZ)
 		{
-			Tetromino block = Tetromino('7');
+			Tetromino block = Tetromino(7);
 			block.rotate_left();
 			std::string expected = "[0,0,0,0,0,0,1,0,0,1,1,0,0,1,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightZ)
 		{
-			Tetromino block = Tetromino('7');
+			Tetromino block = Tetromino(7);
 			block.rotate_right();
 			std::string expected = "[0,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateRightLeftT)
 		{
-			Tetromino block = Tetromino('6');
+			Tetromino block = Tetromino(6);
 			block.rotate_right();
 			block.rotate_left();
 			std::string expected = "[0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateFourT)
 		{
-			Tetromino block = Tetromino('6');
+			Tetromino block = Tetromino(6);
 			block.rotate_right();
 			block.rotate_right();
 			block.rotate_right();
 			block.rotate_right();
 			std::string expected = "[0,0,0,0,0,1,0,0,1,1,1,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(RotateThreeLeftEqualOneRight)
 		{
-			Tetromino block_left = Tetromino('5');
-			Tetromino block_right = Tetromino('5');
+			Tetromino block_left = Tetromino(5);
+			Tetromino block_right = Tetromino(5);
 			block_left.rotate_left();
 			block_left.rotate_left();
 			block_left.rotate_left();
 			block_right.rotate_right();
-			Assert::AreEqual(block_left.toString(), block_right.toString());
+			Assert::AreEqual(toString(block_left), toString(block_right));
 		};
 	};
 	TEST_CLASS(TetrominoTestSetColor)
 	{
 		TEST_METHOD(SetColorOnce)
 		{
-			Tetromino block = Tetromino('6');
-			block.setColor('2');
+			Tetromino block = Tetromino(6);
+			block.setColor(2);
 			std::string expected = "[0,0,0,0,0,2,0,0,2,2,2,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 		TEST_METHOD(DoManyThingsWithMatrix)
 		{
-			Tetromino block = Tetromino('7');
-			block.setColor('2');
+			Tetromino block = Tetromino(7);
+			block.setColor(2);
 			block.rotate_right();
-			block.setColor('3');
+			block.setColor(3);
 			std::string expected = "[0,0,3,0,0,3,3,0,0,3,0,0,0,0,0,0,]";
-			Assert::AreEqual(block.toString(), expected);
+			Assert::AreEqual(toString(block), expected);
 		};
 	};
 }
