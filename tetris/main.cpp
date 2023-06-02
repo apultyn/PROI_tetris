70770eb40d6f27c97c9e985dafaffaef42e6a85f
jkosla <01178539@pw.edu.pl>
2023-06-02 14:12:42 +0200

    Updated main to enable restart, change level or end game

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -13,6 +13,25 @@ int main()
 {
     Window window;
     Menu menu(window);
-    menu.openMenu();
+    int level = menu.openMenu();
+    Game game(window, level);
+    int choice = game.startGame();
+    while (choice != CLOSE)
+    {
+        if (choice == RESTART)
+        {
+            Game game(window, level);
+            choice = game.startGame();
+        }
+        else if (choice == CHANGE_LVL)
+        {
+            Menu menu(window);
+            level = menu.openMenu();
+            Game game(window, level);
+            choice = game.startGame();
+        }
+    }
+    window.getWindow().close();
+
     return 0;
 }
